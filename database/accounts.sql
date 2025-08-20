f455017fea0dc6877e36f450768babe5d0311681
aujiz11 <letuank2018@gmail.com>
2025-08-20 11:41:19 +0700

    get default data when account is created

--- database/accounts.sql
+++ database/accounts.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts`  (
   `id` int NOT NULL AUTO_INCREMENT,
+  `Registered` tinyint NOT NULL DEFAULT 0,
   `Username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `Password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `BirthDate` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '00/00/0000',
@@ -13,5 +14,6 @@ CREATE TABLE `accounts`  (
   `PlayerAngle` float NOT NULL DEFAULT 0,
   `World` int NOT NULL DEFAULT 0,
   `Interior` tinyint NOT NULL DEFAULT 0,
+  `Admin` tinyint NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`, `Username`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
